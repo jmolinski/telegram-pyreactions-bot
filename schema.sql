2572192a4e5fa690cb2767dd708294f30b1f11a8
Jakub Molinski <jakub@molinski.dev>
2021-02-14 05:27:52 +0100

    Schema for reaction and message tables

---
+++ schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS message
+(
+    id              INTEGER PRIMARY KEY,
+    chat_id         INT     NOT NULL,
+    is_reply        BOOLEAN NOT NULL,
+    parent          INT,
+    is_bot_reaction boolean NOT NULL,
+    expanded        boolean not null default FALSE,
+
+    FOREIGN KEY (parent) REFERENCES message (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS reaction
+(
+    id     INTEGER PRIMARY KEY,
+    parent INT,
+    author TEXT NOT NULL,
+    type   TEXT NOT NULL,
+
+    FOREIGN KEY (parent) REFERENCES message (id)
+);
