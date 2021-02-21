6a040cb48dddc77d6a635f11e22cc331a3da0478
Jakub Molinski <jakub@molinski.dev>
2021-02-21 17:55:57 +0100

    Fix normalization for rel reaction

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 EMPTY_MSG = "\xad\xad"
 INFORMATION_EMOJI = "ℹ️"
-TEXTUAL_NORMALIZATION = {"xd": "xD", "rigcz": "RiGCz"}
+TEXTUAL_NORMALIZATION = {"xd": "xD", "rigcz": "RiGCz", "rel": "rel", "rak": "rak"}
 TEXTUAL_REACTIONS = ("+1", "-1", "xD", "rel", "RiGCz", "rak")
