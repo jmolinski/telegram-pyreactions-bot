1c8ab3913fe3ca47c2ca03bef2804abf1053fff3
Jakub Molinski <jakub@molinski.dev>
2022-12-26 19:04:34 +0100

    Optimize db connection creation

--- src/db.py
+++ src/db.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import sqlite3
 
 from contextlib import contextmanager
@@ -6,12 +8,29 @@ from typing import Iterator
 from src import constants
 
 
+CONNECTION: sqlite3.Connection | None = None
+
+
 @contextmanager
 def get_conn() -> Iterator[sqlite3.Connection]:
-    c = sqlite3.connect(constants.DB_FILENAME)
-    yield c
-    c.commit()
-    c.close()
+    global CONNECTION
+    if CONNECTION is None:
+        CONNECTION = sqlite3.connect(constants.DB_FILENAME)
+
+    try:
+        yield CONNECTION
+    except:
+        # TODO handle errors, recreate connection? would the exception be raised here or in .commit?
+        raise
+
+    CONNECTION.commit()
+
+
+def close_conn() -> None:
+    global CONNECTION
+    if CONNECTION is not None:
+        CONNECTION.close()
+        CONNECTION = None
 
 
 with open(constants.SCHEMA_FILENAME) as f:
